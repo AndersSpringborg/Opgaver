248d64e2c2a03fb7a7f6106e6064067e5d03d5b8
888aaen <aaspringborg@gmail.com>
2019-03-13 15:42:57 +0100

    graf ting

--- gui/gui/Class1.cs
+++ gui/gui/Class1.cs
@@ -8,28 +8,9 @@ namespace gui
 {
     class Form1 : Form
     {
-        public Button button1;
         public Form1 ()
         {
-            button1 = new Button();
-            button1.Size = new Size(100, 100);
-            button1.Location = new Point(100, 100);
-            button1.Text = "Press me";
-            Controls.Add(button1);
-            button1.Click += new EventHandler(button1_Click);
-
-            Size = new Size(300, 300);
-        }
-        private void button1_Click (object sender, System.EventArgs e)
-        {
-            // Finder den form hvor knappen bliver trykket på
-            Form myForm = button1.FindForm();
-            // Laver titlen på programmet om
-            myForm.Text = "The Form of My Control";
-            //Laver baggrundsfarven om
-            myForm.BackColor = Color.Red;
-            //Laver en textbox, ligesom fejlbox
-            MessageBox.Show("hej bøsser");
+            ValueTuple.Create();
         }
     }
 }
