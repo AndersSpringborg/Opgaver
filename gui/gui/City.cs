a24be23ed537e51edea0df9f96958c1cca83c3a3
888aaen <aaspringborg@gmail.com>
2019-03-13 17:27:11 +0100

    oprettet byer

---
+++ gui/gui/City.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace gui
+{
+    class City
+    {
+        public City (string name, int x, int y)
+        {
+            this.Name = name;
+            //this.Size = size;
+            this.CoordinateX = x;
+            this.CoordinateY = y;
+        }
+        public string Name {get; set;}
+        //public int Size {get; set;}
+        public int CoordinateX {get; set;}
+        public int CoordinateY {get; set;}
+    }
+}
--- gui/gui/Class1.cs
+++ gui/gui/Class1.cs
@@ -10,7 +10,25 @@ namespace gui
     {
         public Form1 ()
         {
-            ValueTuple.Create();
+            City Aalborg = new City("Aalborg", 150, 200);
+            City Aarhus = new City("Aarhus", 150, 350);
+            City Frederikshavn = new City("Frederikshavn", 250, 200);
+
+            this.Controls.Add(NewButton(Aalborg));
+            this.Controls.Add(NewButton(Aarhus));
+            this.Controls.Add(NewButton(Frederikshavn));
+
+            // Item1 = by1     |      Item2 = vægt      |   Item3 = by2;
+            var Relationer = new List<Tuple<City, int, City>>();
+        }
+        
+        private Button NewButton(City NewCity)
+        {
+            Button NewButton = new Button();
+            NewButton.Location = new Point(NewCity.CoordinateX, NewCity.CoordinateY);
+            NewButton.Size = new Size(100, 100);
+            NewButton.Text = NewCity.Name;
+            return NewButton;
         }
     }
 }
