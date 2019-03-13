00e826d5736bf6e32506c99e01f769d548e793be
888aaen <aaspringborg@gmail.com>
2019-03-13 17:31:35 +0100

    kommentare

--- gui/gui/Class1.cs
+++ gui/gui/Class1.cs
@@ -10,10 +10,12 @@ namespace gui
     {
         public Form1 ()
         {
+            //Her oprettes vores byer (Database)
             City Aalborg = new City("Aalborg", 150, 200);
             City Aarhus = new City("Aarhus", 150, 350);
             City Frederikshavn = new City("Frederikshavn", 250, 200);
 
+            //Opretter buttons for byerne
             this.Controls.Add(NewButton(Aalborg));
             this.Controls.Add(NewButton(Aarhus));
             this.Controls.Add(NewButton(Frederikshavn));
@@ -22,6 +24,7 @@ namespace gui
             var Relationer = new List<Tuple<City, int, City>>();
         }
         
+        //Laver en knude i form af en button, og returnere den
         private Button NewButton(City NewCity)
         {
             Button NewButton = new Button();
