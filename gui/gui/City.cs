4b72804d12b5bb371304c1b13f9cd417547182cf
Anders Aaen Springborg <asprin18@student.aau.dk>
2019-03-20 15:02:21 +0100

    Inden lektiecafe

--- gui/gui/City.cs
+++ gui/gui/City.cs
@@ -8,16 +8,18 @@ namespace gui
 {
     class City
     {
-        public City (string name, int x, int y)
+        public City(string name, int x, int y)
         {
             this.Name = name;
             //this.Size = size;
             this.CoordinateX = x;
             this.CoordinateY = y;
         }
-        public string Name {get; set;}
+        public string Name { get; set; }
         //public int Size {get; set;}
-        public int CoordinateX {get; set;}
-        public int CoordinateY {get; set;}
+        public int CoordinateX { get; set; }
+        public int CoordinateY { get; set; }
+        public List<Tuple<City, int, City>> Adjacent = new List<Tuple<City, int, City>>();
+        
     }
 }
--- gui/gui/Class1.cs
+++ gui/gui/Class1.cs
@@ -6,22 +6,39 @@ using System.Drawing;
 
 namespace gui
 {
-    class Form1 : Form
+    public partial class Form1 : Form
     {
         public Form1 ()
         {
+            
+    
+
+
             //Her oprettes vores byer (Database)
             City Aalborg = new City("Aalborg", 150, 200);
             City Aarhus = new City("Aarhus", 150, 350);
             City Frederikshavn = new City("Frederikshavn", 250, 200);
+                
+            // Item1 = by1     |      Item2 = vægt      |   Item3 = by2;
+            var Relations = new List<Tuple<City, int, City>>();
+            Relations.Add(new Tuple<City, int, City>(Aalborg, 1, Aarhus));
+            Relations.Add(new Tuple<City, int, City>(Aalborg, 1, Frederikshavn));
+
+            foreach(Tuple<City, int, City> item in Relations)
+            {
+
+                //item.Item1.Adjacent.Add(item);
+                //item.Item3.Adjacent.Add(item);
+            }
+
+
+
 
             //Opretter buttons for byerne
             this.Controls.Add(NewButton(Aalborg));
             this.Controls.Add(NewButton(Aarhus));
             this.Controls.Add(NewButton(Frederikshavn));
 
-            // Item1 = by1     |      Item2 = vægt      |   Item3 = by2;
-            var Relationer = new List<Tuple<City, int, City>>();
         }
         
         //Laver en knude i form af en button, og returnere den
@@ -29,9 +46,21 @@ namespace gui
         {
             Button NewButton = new Button();
             NewButton.Location = new Point(NewCity.CoordinateX, NewCity.CoordinateY);
-            NewButton.Size = new Size(100, 100);
+            NewButton.Size = new Size(75, 75);
             NewButton.Text = NewCity.Name;
             return NewButton;
+        }
+
+        private void DrawImagePointF(PaintEventArgs e)
+        {
+            Point[] points =
+            {
+            new Point(100,100),
+            new Point(100,200),
+            new Point(200,100)
+        };
+
+            e.Graphics.DrawLines(new Pen(Color.Red, 8), points);
         }
     }
 }
