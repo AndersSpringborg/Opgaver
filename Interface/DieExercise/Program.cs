7e10cbb54fccd652c2672564ee5235158f2458a6
Anders Aaen Springborg <asprin18@student.aau.dk>
2019-03-21 14:56:27 +0100

    die opgave,  jeg fatter ikke noget

---
+++ Interface/DieExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace DieExercise
+{
+    public class Die : IRandom
+    {
+        private int numberOfEyes;
+        private Random randomNumberSupplier;
+        public const int Sides = 6;
+
+        public Die(Random randomGenerator)
+        {
+            randomNumberSupplier = randomGenerator;
+            numberOfEyes = NewTossHowManyEyes();
+        }
+
+        public void Toss()
+        {
+            numberOfEyes = NewTossHowManyEyes();
+        }
+
+        private int NewTossHowManyEyes()
+        {
+            return randomNumberSupplier.Next(1, Sides + 1);
+        }
+
+        public int NumberOfEyes()
+        {
+            return numberOfEyes;
+        }
+
+        public override String ToString()
+        {
+            return String.Format("[{0}]", numberOfEyes);
+        }
+    }
+}
