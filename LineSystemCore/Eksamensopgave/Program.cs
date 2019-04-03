b909580a1b197e6d11ce041a48d3c113a58319d4
Anders Aaen Springborg <asprin18@student.aau.dk>
2019-04-03 15:49:04 +0200

    Opstart

---
+++ LineSystemCore/Eksamensopgave/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksamensopgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                User kurt = new User(1, "Anders", "Springborg", Console.ReadLine());
+                Console.WriteLine(kurt.UserName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("hej");
+                throw;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LineSystemCore/Eksamensopgave/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksamensopgave
+{
+    class User
+    {
+        private string _userName;
+        public uint ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                if (_userName.All(Char.IsLetterOrDigit))
+                    UserName = _userName;
+                else
+                    new Exception();
+            }
+        }
+
+        public User(uint iD, string firstName, string lastName, string userName)
+        {
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            _userName = userName;
+        }
+    }
+}
