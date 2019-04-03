d0c29eecf70ba45881c76d59fdedad16d76ff62f
Anders Aaen Springborg <asprin18@student.aau.dk>
2019-04-03 15:53:04 +0200

    Opstart

---
+++ LineSystem/LineSystemCore/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineSystemCore
+{
+    class User
+    {
+        private string _userName;
+        public uint ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                if (_userName.All(Char.IsLetterOrDigit))
+                    UserName = _userName;
+                else
+                    new Exception();
+            }
+        }
+
+        public User(uint iD, string firstName, string lastName, string userName)
+        {
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            _userName = userName;
+        }
+    }
+}
