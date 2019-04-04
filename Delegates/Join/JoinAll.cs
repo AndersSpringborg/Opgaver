5321d6c5c08bc124935655bd856d820010fff348
Anders Aaen Springborg <aaspringborg@gmail.com>
2019-04-04 14:37:10 +0200

    Kneppet delgates

---
+++ Delegates/Join/JoinAll.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Join
+{
+
+    class JoinAll
+    {
+        public delegate string StringJoin(string l, string r);
+
+        public StringJoin Concat { get => ConcatString; }
+
+        public T JoinEver<T>(List<T> list, Func<T, T, T> function)
+        {
+            T result = list[0];
+            foreach (T item in list.Skip(1))
+            {
+                result = function(result, item);
+            }
+            return result;
+        }
+        private string ConcatString(string l, string r) => l + r;
+        public string JoinThree(string str1, string str2, string str3, StringJoin function) => function(function(str1, str2), str3);
+        public string JoinAllStrings(List<string> input, StringJoin function)
+        {
+            string result = input[0];
+            for (int i = 1; i < input.Count; i++)
+            {
+                result = function(result, input[i]);
+            }
+            return result;
+        }
+    }
+}
---
+++ Delegates/Join/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Join
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Join = new JoinAll();
+            
+            
+            Console.WriteLine(Join.Concat("hej ", "med dig"));
+
+            Console.WriteLine(Join.JoinThree("a", "b", "c", (l, r) => l + r));
+            Console.WriteLine(Join.JoinThree("a", "b", "c", (l, r) => l + "." + r));
+            Console.WriteLine(Join.JoinThree("a", "b", "c", (l, r) => l));
+
+
+            var list = new List<string>() { "a", "b", "c", "d", "e" };
+            Console.WriteLine(Join.JoinAllStrings(list, (l,r) => l + "." + r));
+            Console.WriteLine(Join.JoinAllStrings(list, (l, r) => l + r));
+            Console.WriteLine(Join.JoinAllStrings(list, (l, r) => r));
+
+            List<int> list2 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            Console.WriteLine(Join.JoinEver(list2, (v,b) => v + b ));
+            Console.WriteLine(Join.JoinEver(list, (v, b) => b + "." + v));
+
+            Console.ReadKey();
+        }
+
+    }
+}
